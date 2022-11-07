f45ac99490a551c0d12df02ee04e009dfc1bed65
jcg0 <jg.awsprod@gmail.com>
2022-11-06 21:29:29 -0500

    moved question and answers to an array of objects. created checkAnswer function which is where answer validation occurs

--- assets/app.js
+++ assets/app.js
@@ -4,27 +4,44 @@ const quizSection = document.querySelector("#quizSection");
 const ol = document.querySelector("#ol");
 const span = document.querySelector("#span");
 
-let answers = [
-  "javascript answer 1 javascript answer 1 javascript answer 1 javascript answer 1 javascript answer 1",
-  "javascript answer 1 javascript answer 1 javascript answer 1 javascript answer 1 javascript answer 1",
-  "javascript answer 1 javascript answer 1 javascript answer 1 javascript answer 1 javascript answer 1",
-  "javascript answer 1 javascript answer 1 javascript answer 1 javascript answer 1 javascript answer 1",
+let questionCount = 0;
+
+let questionTemplate = [
+  {
+    question: "question one",
+    selection: ["answer 1", "answer 2", "answer 3", "answer 4"],
+    answer: "answer 3",
+  },
+  {
+    question: "question two",
+    selection: ["answer 1", "answer 2", "answer 3", "answer 4"],
+    answer: "answer 2",
+  },
 ];
 
-let questions = [
-  "question 1 question 1 question 1 v v v vquestion 1 question 1 ",
-  "question 2",
-  "question 3",
-  "question 4",
-];
-
-startBtn.addEventListener("click", function () {
-  quizSection.remove();
-  span.innerText = questions[0];
-  span.style.fontSize = "150%";
-  for (let i = 0; i < 4; i++) {
-    let li = document.createElement("li");
-    li.textContent = answers[i];
-    ol.append(li);
+function checkAnswer(event) {
+  console.log(event.target.innerText);
+  console.log(questionTemplate[questionCount].answer);
+  if (event.target.innerText === questionTemplate[questionCount].answer) {
+    console.log("Correct!");
+  } else {
+    console.log("Sorry that is incorrect.");
   }
-});
+}
+
+function startGame() {
+  startBtn.addEventListener("click", function () {
+    quizSection.remove();
+    span.innerText = questionTemplate[questionCount].question;
+    span.style.fontSize = "150%";
+
+    for (let i = 0; i < questionTemplate[questionCount].selection.length; i++) {
+      let li = document.createElement("li");
+      li.textContent = questionTemplate[questionCount].selection[i];
+      li.addEventListener("click", checkAnswer);
+      ol.append(li);
+    }
+  });
+}
+
+startGame();
