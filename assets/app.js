61379435df95d6f54ed323916c7691bae3a4205a
jcg0 <jg.awsprod@gmail.com>
2022-11-10 21:01:55 -0500

    refactored some code into new functions and then passed those functions along to make the code flow better. changed the li selections to buttons

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,13 @@
 const startBtn = document.querySelector("#startBtn");
 const mainContainer = document.querySelector("#container");
 const landingSection = document.querySelector("#landingSection");
-const ol = document.querySelector("#ol");
+const olEl = document.querySelector("#ol");
 const span = document.querySelector("#span");
 const validation = document.querySelector("#validation");
 const quizSection = document.querySelector("#quizSection");
 const div = document.querySelector("#div");
+const highScore = document.querySelector("#high-score");
+const form = document.querySelector("#form");
 let scoreEl = document.querySelector("#score");
 let timerEl = document.querySelector("#timer");
 
@@ -37,67 +39,134 @@ let questionTemplate = [
     selection: ["answer 13", "answer 14", "answer 15", "answer 16"],
     answer: "answer 16",
   },
+  {
+    question: "question five",
+    selection: ["answer 13", "answer 14", "answer 15", "answer 16"],
+    answer: "answer 16",
+  },
+  {
+    question: "question six",
+    selection: ["answer 13", "answer 14", "answer 15", "answer 16"],
+    answer: "answer 16",
+  },
+  {
+    question: "question seven",
+    selection: ["answer 13", "answer 14", "answer 15", "answer 16"],
+    answer: "answer 16",
+  },
+  {
+    question: "question eight",
+    selection: ["answer 13", "answer 14", "answer 15", "answer 16"],
+    answer: "answer 16",
+  },
+  {
+    question: "question nine",
+    selection: ["answer 13", "answer 14", "answer 15", "answer 16"],
+    answer: "answer 16",
+  },
+  {
+    question: "question ten",
+    selection: ["answer 13", "answer 14", "answer 15", "answer 16"],
+    answer: "answer 16",
+  },
 ];
 
 function checkAnswer(event) {
-  if (event.target.innerText === questionTemplate[questionCount].answer) {
+  // console.log(event.target.innerText);
+  var selectionBtn = event.target;
+  if (!selectionBtn.matches(".selection")) {
+    return;
+  }
+  if (selectionBtn.value !== questionTemplate[questionCount].answer) {
+    console.log(false);
+    timer -= 5;
+    timerEl.textContent = timer;
+    validation.style.color = "red";
+    validation.innerText = "Incorrect";
+  } else {
     console.log(true);
     scoreEl.innerText = ++score;
     validation.style.color = "green";
     validation.innerText = "Correct!";
-  } else {
-    console.log(false);
-    validation.style.color = "red";
-    validation.innerText = "Incorrect";
-    timer.innerText = timer - 5;
   }
-
   questionCount++;
-  for (let i = 0; i < questionTemplate.length; i++) {
-    span.innerText = questionTemplate[questionCount].question;
-    ol.childNodes[0].innerText = questionTemplate[questionCount].selection[0];
-    ol.childNodes[1].innerText = questionTemplate[questionCount].selection[1];
-    ol.childNodes[2].innerText = questionTemplate[questionCount].selection[2];
-    ol.childNodes[3].innerText = questionTemplate[questionCount].selection[3];
-  }
 
-  if (questionCount === questionTemplate[questionCount].question) {
+  if (questionCount === questionTemplate[questionCount].length || timer <= 0) {
     endQuiz();
-    return;
+  } else {
+    getQuestions();
   }
 }
 
+// for (let i = 0; i < questionTemplate.length; i++) {
+//   span.innerText = questionTemplate[questionCount].question;
+//   ol.childNodes[0].innerText = questionTemplate[questionCount].selection[0];
+//   ol.childNodes[1].innerText = questionTemplate[questionCount].selection[1];
+//   ol.childNodes[2].innerText = questionTemplate[questionCount].selection[2];
+//   ol.childNodes[3].innerText = questionTemplate[questionCount].selection[3];
+// }
+
 function endQuiz() {
   clearInterval(timerInterval);
-  if (questionCount === questionTemplate[questionCount].question) {
-    div.innerText = `quiz over here is you score: ${score}`;
+  quizSection.remove();
+  div.innerText = `Quiz over. Here is your score: ${score}`;
+}
+
+function getQuestions() {
+  span.innerText = questionTemplate[questionCount].question;
+  scoreEl.innerText = `Score: ${score}`;
+  span.style.fontSize = "150%";
+
+  olEl.innerHTML = "";
+
+  for (let i = 0; i < questionTemplate[questionCount].selection.length; i++) {
+    var selection = questionTemplate[questionCount].selection[i];
+    let btn = document.createElement("button");
+    // btn.innerText = questionTemplate[questionCount].selection[i];
+
+    btn.setAttribute("class", "selection");
+    btn.setAttribute("value", selection);
+    btn.classList.add("btn");
+
+    // btn.addEventListener("click", checkAnswer);
+    btn.textContent = i + 1 + ". " + selection;
+    olEl.append(btn);
+  }
+  // checkAnswer();
+}
+
+function clock() {
+  timer--;
+  timerEl.textContent = timer;
+  if (timer < 0) {
+    endQuiz();
   }
 }
 
 function startGame() {
   startBtn.addEventListener("click", function () {
     landingSection.remove();
-    span.innerText = questionTemplate[questionCount].question;
-    scoreEl.innerText = `Score: ${score}`;
-    span.style.fontSize = "150%";
+    getQuestions();
 
-    for (let i = 0; i < questionTemplate[questionCount].selection.length; i++) {
-      let li = document.createElement("li");
-      li.innerText = questionTemplate[questionCount].selection[i];
-      li.addEventListener("click", checkAnswer);
-      ol.append(li);
-    }
+    // timerInterval = setInterval(function () {
+    //   timer--;
+    //   timerEl.innerText = timer;
+    //   if (!timer) {
+    //     clearInterval(timerInterval);
+    //     quizSection.remove();
+    //     div.innerText = `Game Over. Here is your score: ${score}`;
+    //     highScore.innerText = score;
+    //     let input = document.createElement("input");
+    //     form.append(input);
 
-    timerInterval = setInterval(function () {
-      timer--;
-      timerEl.innerText = timer;
-      if (!timer) {
-        clearInterval(timerInterval);
-        quizSection.remove();
-        div.innerText = `Game Over. Here is your score: ${score}`;
-      }
-    }, 1000);
+    //     localStorage.getItem("high score", score);
+    //     localStorage.getItem("input", input);
+    //   }
+    // }, 1000);
+    timerInterval = setInterval(clock, 1000);
   });
 }
+
+olEl.onclick = checkAnswer;
 
 startGame();
